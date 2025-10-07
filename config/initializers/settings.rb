8ecc1fd51cebd7d696f1516cc64dbde0cd4172e5
David Afonso <davafons@gmail.com>
2025-10-07 23:52:55 +0900

    Setup prod specific credentials

--- config/initializers/settings.rb
+++
@@ -1,4 +0,0 @@
-# Sync environment variables to Settings on app initialization
-Rails.application.config.after_initialize do
-  Setting.sync_from_env! if ActiveRecord::Base.connection.table_exists?("settings")
-end
