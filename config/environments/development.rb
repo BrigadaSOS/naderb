2475717489159ea2b10f30f0424df28ab84a80d2
David Afonso <davafons@gmail.com>
2025-10-08 02:03:38 +0900

    Make Solid Queue work in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
   end
 
-  # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  # Use file store to share cache between processes (web + worker)
+  config.cache_store = :file_store, Rails.root.join("tmp/cache")
 
   # Use Solid Queue in development to match production
   config.active_job.queue_adapter = :solid_queue
